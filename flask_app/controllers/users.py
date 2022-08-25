b810dfac0f196d6e9c1747e64ee2752d26605b98
Bearbeat16 <Jadamsfletcher@gmail.com>
2022-08-24 17:36:28 -0700

    First Commit

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,5 @@
+import re
+from flask_app import app, Bcrypt
+from flask import render_template,redirect,request,session,flash
+from flask_app.models.user import User
+from flask_app.models.painting import Painting
